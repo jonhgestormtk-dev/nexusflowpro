c3843223b68bf6bb8261ddcdddfc50b06eff1dd1
Jonh Quadros <jonhgestor.mtk@gmail.com>
2026-05-07 18:46:47 +0000

    Se você já estiver usando o npm e um bundler de módulos, como webpack ou

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8792097810-ef170",
-  "appId": "1:694170350964:web:7dff8d72b5553543ede821",
-  "apiKey": "AIzaSyDuPIDWfMTaNzSRuNnz5_NeI_e3Hmrbf8M",
-  "authDomain": "studio-8792097810-ef170.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "694170350964"
+  apiKey: "AIzaSyDuPIDWfMTaNzSRuNnz5_NeI_e3Hmrbf8M",
+  authDomain: "studio-8792097810-ef170.firebaseapp.com",
+  projectId: "studio-8792097810-ef170",
+  storageBucket: "studio-8792097810-ef170.firebasestorage.app",
+  messagingSenderId: "694170350964",
+  appId: "1:694170350964:web:7dff8d72b5553543ede821"
 };
